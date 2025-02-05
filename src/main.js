f6ec936f33cd62d210e498c6402d3cdc10ec6291
Aydenpitstick <108236392+Aydenpitstick@users.noreply.github.com>
2025-02-05 13:38:16 -0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import web from './web.vue'
+
+createApp(web).mount('#app')
---
+++ src/web.vue
@@ -0,0 +1,48 @@
+<script setup>
+import {ref} from 'vue';
+
+let bread = ref(0);
+let eggs = ref(0);
+let milk = ref(0);
+
+function breadIncrease(){
+    bread.value++
+    console.log(bread.value)
+}
+
+function breadDecrease(){
+    bread.value--
+    console.log(bread.value)
+}
+function eggsIncrease(){
+    eggs.value++
+    console.log(eggs.value)
+}
+function eggsDecrease(){
+    eggs.value--
+    console.log(eggs.value)
+}
+function milkIncrease(){
+    milk.value++
+    console.log(milk.value)
+}
+function milkDecrease(){
+    milk.value--
+    console.log(milk.value)
+}
+</script>
+
+<template>
+Bread <button @click="breadIncrease">+</button>/<button @click="breadDecrease">-</button>
+{{ bread }}
+<div>
+</div>
+Eggs <button @click="eggsIncrease">+</button>/<button @click="eggsDecrease">-</button>
+ {{ eggs }}
+ <div>
+</div>
+Milk <button @click="milkIncrease">+</button>/<button @click="milkDecrease">-</button>
+{{ milk }}
+<div>
+</div>
+</template>
